eba147b045a7ac469695e66a34d72d2a6efa2cef
Locrian <info@wemudtogether.com>
2026-01-11 20:26:59 -0600

    Adding script to index.php

--- index.php
+++ index.php
@@ -139,4 +139,8 @@ include 'includes/header.php';
     </article>
   </main>
 
-<?php include 'includes/footer.php'; ?>
+  <?php include 'includes/footer.php'; ?>
+  <script src="/design/script/script_main.js"></script>
+</body>
+
+</html>

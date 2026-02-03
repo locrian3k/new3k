bac7125af8ea0528e1c7e141f9efa0a689cd93b5
Locrian <info@wemudtogether.com>
2026-02-02 20:29:19 -0600

    link fix

--- about/3kingdoms/index.php
+++ about/3kingdoms/index.php
@@ -27,7 +27,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 			<section class="about-intro">
 				<div class="about-container">
 					<div class="pinnacle-image-container">
-						<a href="/images/pinnacle.jpg" target="_blank">
+						<a href="/images/ai-areas/pinnacle.png" target="_blank">
 							<img
 								src="/images/ai-areas/pinnacle.png"
 								alt="The mighty town of Pinnacle"

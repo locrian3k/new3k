b7fa138c7a0ce73911608237d217937dca80cefd
Locrian <info@wemudtogether.com>
2026-02-09 21:21:31 -0600

    adding primer to footer

--- includes/footer.php
+++ includes/footer.php
@@ -42,6 +42,7 @@
               <li><a href="/support/contact/index.php">Contact</a></li>
               <li><a href="/support/help/index.php">Help Files</a></li>
               <li><a href="/support/vafs/index.php">VAFs (Support Us)</a></li>
+              <li><a href="/resources/primer/index.php">3K Codong Primer</a></li>
             </ul>
           </div>          
 		    </div>
